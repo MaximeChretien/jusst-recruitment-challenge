aa437475ec6ea62a0d752c23093b8965be05798b
Maxime Chretien <mchretien@linuxmail.org>
2022-07-26 19:22:34 +0000

    Implement Json parser
    Signed-off-by: Maxime Chretien <mchretien@linuxmail.org>

--- src/main.cpp
+++ src/main.cpp
@@ -3,13 +3,14 @@
  *    @mail    : mchretien@linuxmail.org
  *    @project : jusst-recruitment-challenge
  *    @summary : Juust recruitement challenge for embedded dev
- *    @version : 0.1
+ *    @version : 0.2
  */
 
 #include <boost/beast/core.hpp>
 #include <boost/beast/websocket.hpp>
 #include <boost/asio/connect.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/json.hpp>
 #include <cstdlib>
 #include <iostream>
 #include <string>
@@ -19,6 +20,7 @@ namespace beast = boost::beast;         // from <boost/beast.hpp>
 namespace http = beast::http;           // from <boost/beast/http.hpp>
 namespace websocket = beast::websocket; // from <boost/beast/websocket.hpp>
 namespace net = boost::asio;            // from <boost/asio.hpp>
+namespace json = boost::json;            // from <boost/json.hpp>
 using tcp = boost::asio::ip::tcp;       // from <boost/asio/ip/tcp.hpp>
 
 volatile sig_atomic_t stop;
@@ -27,7 +29,39 @@ void int_handler(int signum) {
     stop = 1;
 }
 
-// Sends a WebSocket message and prints the response
+struct state {
+	std::string system;
+	std::string playback;
+	int volume;
+	std::string bluetooth;
+};
+
+// Function parsing json message and updating the struct
+void json_parser(std::string json_str, struct state* dev_state) {
+	// Parse json
+	json::value parsed_json = json::parse(json_str);
+	json::object json_object = parsed_json.as_object();
+
+	// Iterate the json object
+	for (auto val_key_pair: json_object) {
+		std::string key = val_key_pair.key();
+
+		json::value val = val_key_pair.value();
+
+		// Update the struct
+		if (key == "system") {
+			dev_state->system = val.as_string();
+		} else if (key == "playback") {
+			dev_state->playback = val.as_string();
+		} else if (key == "volume") {
+			dev_state->volume = val.as_int64();
+		} else if (key == "bluetooth") {
+			dev_state->bluetooth = val.as_string();
+		}
+	}
+
+}
+
 int main(int argc, char** argv)
 {
 	signal(SIGINT, int_handler);
@@ -37,9 +71,11 @@ int main(int argc, char** argv)
 
 	// Set server settings
 	std::string host = "localhost";
-	std::string const  port = "8808";
-	std::string const  target = "/ws";
+	std::string const port = "8808";
+	std::string const target = "/ws";
 
+	// State stucture to hold values decoded from the websocket
+	struct state device_state;
 
 	try
 	{
@@ -78,11 +114,19 @@ int main(int argc, char** argv)
 			// Read a message into our buffer
 			ws.read(buffer);
 
-			// Print the message
-			std::cout << beast::make_printable(buffer.data()) << std::endl;
+			// Parse json message
+			json_parser(beast::buffers_to_string(buffer.data()), &device_state);
 
 			// Clear the buffer
 			buffer.clear();
+
+			// Print the current state
+			std::cout << "State :" << std::endl;
+			std::cout << "  system : " << device_state.system << std::endl;
+			std::cout << "  playback : " << device_state.playback << std::endl;
+			std::cout << "  volume : " << device_state.volume << std::endl;
+			std::cout << "  bluetooth : " << device_state.bluetooth << std::endl;
+			std::cout << std::endl;
 		};
 
 		// Close the WebSocket connection
